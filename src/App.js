0f00c3f88443219efad9b10ce71b558fb01d6077
Bugas <bugasmarcondes@MacBook-Pro-de-Bugas.local>
2020-02-25 15:09:05 -0300

    Criando o Dashboard e importando os componentes Notifications e ProjectList

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Switch, Route } from "react-router-dom";
 import Navbar from "./components/layout/Navbar";
+import Dashboard from "./components/dashboard/Dashboard";
 
 function App() {
   return (
     <BrowserRouter>
       <div className="App">
         <Navbar />
+        {/* o Switch garante que somente uma rota será carregada */}
+        <Switch>
+          <Route path="/" component={Dashboard} />
+        </Switch>
       </div>
     </BrowserRouter>
   );
