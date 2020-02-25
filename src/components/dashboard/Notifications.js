aa715380ae405c5ba927b9e820e445e20dd1def2
Bugas <bugasmarcondes@MacBook-Pro-de-Bugas.local>
2020-02-25 16:15:18 -0300

    - Montando CSS do componente Notifications - Criação da cloud function para inserir Notificações na collection firestore

--- src/components/dashboard/Notifications.js
+++ src/components/dashboard/Notifications.js
@@ -2,8 +2,18 @@ import React from "react";
 
 const Notifications = () => {
   return (
-    <div>
-      <p>Notifications</p>
+    <div className="section">
+      <div className="card z-depth-0">
+        <div className="card-content">
+          <span className="card-title">Notifications</span>
+          <ul className="notifications">
+            <li>1</li>
+            <li>2</li>
+            <li>3</li>
+            <li>4</li>
+          </ul>
+        </div>
+      </div>
     </div>
   );
 };
