c1126d0df0099d5bf8fff47dee6174aefbb560b0
Bugas <bugasmarcondes@MacBook-Pro-de-Bugas.local>
2020-02-25 16:13:25 -0300

    - Instalando dependências para usar cloud functions - Hello World cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,43 +1,8 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
+const functions = require("firebase-functions");
 
-// permite utilização de authentication e firebase services
-admin.initializeApp(functions.config().firebase);
-
+// // Create and Deploy Your First Cloud Functions
+// // https://firebase.google.com/docs/functions/write-firebase-functions
+//
 exports.helloWorld = functions.https.onRequest((request, response) => {
-    response.send("Hello from Bugas!");
-});
-
-const createNotification = (notification) => {
-    return admin.firestore()
-        .collection('notifications')
-        .add(notification)
-        .then(doc => console.log('notification added: ' + doc));
-}
-
-exports.projectCreated = functions.firestore
-    .document('/projects/{projectId}')
-    .onCreate(doc => {
-        const project = doc.data();
-        const notification = {
-            content: 'Added a new project',
-            user: `${project.authorFirstName} ${project.authorLastName}`,
-            time: admin.firestore.FieldValue.serverTimestamp()
-        };
-
-    return createNotification(notification);
-});
-
-exports.userJoined = functions.auth.user().onCreate(user => {
-    // eslint-disable-next-line promise/always-return
-    return admin.firestore().collection('users').doc(user.uid).get().then(doc => {
-        const newUser = doc.data();
-        const notification = {
-            content: 'Joined the party',
-            user: `${newUser.firstName} ${newUser.lastName}`,
-            time: admin.firestore.FieldValue.serverTimestamp()
-        };
-
-        return createNotification(notification);
-    });
+  response.send("Hello from Bugas!");
 });
