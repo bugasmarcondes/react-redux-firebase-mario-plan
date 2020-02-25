16b5807e50afa80c833bead69d090356e1b9800f
Bugas <bugasmarcondes@MacBook-Pro-de-Bugas.local>
2020-02-25 15:53:16 -0300

    - Criando e aplicando ação para realizar SignOut

--- src/components/layout/SignedInLinks.js
+++ src/components/layout/SignedInLinks.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { NavLink } from "react-router-dom"; //monta urls com funcionalidade ativo/desativado
+import { connect } from "react-redux";
+import { signOut } from "../../store/actions/authActions";
 
 // rafc for autocomplete
-const SignedInLinks = () => {
+const SignedInLinks = props => {
   return (
     <ul className="right">
       <li>
         <NavLink to="/create">New Project</NavLink>
       </li>
       <li>
-        <NavLink to="/">Log Out</NavLink>
+        <a onClick={props.signOut}>Log Out</a>
       </li>
       <li>
         <NavLink to="/" className="btn btn-floating pink lighten-1">
@@ -20,4 +22,10 @@ const SignedInLinks = () => {
   );
 };
 
-export default SignedInLinks;
+const mapDispatchToProps = dispatch => {
+  return {
+    signOut: () => dispatch(signOut())
+  };
+};
+
+export default connect(null, mapDispatchToProps)(SignedInLinks);
